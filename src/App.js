77ab7c08c1afa00c13b50663ecf474aa88b90906
nitesh5801 <67116285+nitesh5801@users.noreply.github.com>
2020-12-22 19:30:29 +0530

    material ui card added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar"; 
 import MyCard from "./components/MyCard";
 import { getMatches } from "./api/Api";
-import React, { useEffect } from "react";
-
+import { Container, Grid, Typography } from "@material-ui/core";
 function App() {
+  const [matches, setMatches] = useState([]);
+
   useEffect(() => {
     getMatches()
-      .then((data) => console.log("DATA", data))
-      .catch();
+      .then((data) => {
+        console.log(data.matches);
+        setMatches(data.matches);
+      })
+      .catch((error) => {});
   }, []);
 
   return (
     <div className="App">
-      <Navbar />
-      <h1>Welcome to my Live Score APP</h1>
-      <MyCard />
-      {}
+      <Navbar/>
+      <Typography variant="h3" align="center" style={{marginTop:20}}>Welcome to my Live Score APP</Typography>
+      <Container>
+        <Grid container>
+          <Grid item xs={12}>
+            {matches.map((match) => (
+              <React.Fragment>
+              {
+                match.type=="Twenty20"?(<MyCard key={match.unique_id} match={match}/>):("")
+              }
+              </React.Fragment>
+            ))}
+          </Grid>
+        </Grid>
+      </Container>
     </div>
   );
 }
--- src/api/Api.jsx
+++ src/api/Api.jsx
@@ -9,3 +9,12 @@ export const getMatches = () => {
     .then((response) => response.json())
     .catch((error) => console.log("ERROR : ", error));
 };
+
+//get the score of the cuurent match
+
+export const getMatchDetail = (id) => {
+  const url = `https://cricapi.com/api/cricketScore?unique_id=${id}&apikey=${API_KEY}`;
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.log(error));
+};
